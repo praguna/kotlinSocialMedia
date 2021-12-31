ec28a35ecaed660e24cf7adc923979f2336158cc
praguna <praguna.manvi@gmail.com>
2021-12-31 13:47:37 +0530

    pointing to the server

--- blogsite/src/services/auth.service.js
+++ blogsite/src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/auth/";
+const API_URL = "http://bloggerservice-dev.us-west-2.elasticbeanstalk.com/api/auth/";
 
 // const sampleData = {
 //   username: "NITISH",
